39719dd9a3526a830bbbb0549aa0d76ad328cac8
Mandu <54462907+kartikamilenda@users.noreply.github.com>
2021-03-17 12:28:20 +0700

    Membuat class ColorListener tapi belum beres

---
+++ MoveCircle/MoveCircle.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MoveCircle;
+import java.awt.*; 
+import javax.swing.*; 
+/**
+ *
+ * @author asus
+ */
+public class MoveCircle { 
+ //----------------------------------- 
+ // Set up a frame for the GUI. 
+ //----------------------------------- 
+ public static void main(String[] args) 
+ { 
+ JFrame frame = new JFrame ("MoveCircle"); 
+ frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+ frame.setPreferredSize(new Dimension(400,300)); 
+ frame.getContentPane().add(new CirclePanel(400,300)); 
+ frame.pack(); 
+ frame.setVisible(true); 
+ } 
+}
