a5b9767f720e6b59c3ebaac23158467806def653
Mandu <54462907+kartikamilenda@users.noreply.github.com>
2021-03-22 14:58:07 +0700

    menyimpan try diluar loop

--- PlacingExceptionHandlers/ParseInts.java
+++ PlacingExceptionHandlers/ParseInts.java
@@ -16,12 +16,15 @@ public class ParseInts {
     Scanner scan = new Scanner(System.in); 
     String line; 
     System.out.println("Enter a line of text"); 
-    Scanner scanLine = new Scanner(scan.nextLine()); 
-    while (scanLine.hasNext()) 
-    { 
-        val = Integer.parseInt(scanLine.next()); 
-        sum += val; 
-    } 
-    System.out.println("The sum of the integers on this line is " + sum); 
- }
+    Scanner scanLine = new Scanner(scan.nextLine());
+    try{
+        while (scanLine.hasNext()) 
+        { 
+            val = Integer.parseInt(scanLine.next()); 
+            sum += val; 
+        }  
+       }   catch(NumberFormatException exception){
+       }
+        System.out.println("The sum of the integers on this line is " + sum); 
+  }
 }
