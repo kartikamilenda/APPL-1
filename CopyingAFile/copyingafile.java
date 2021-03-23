d7c109d3ec396f3ebd9bd94761037e29b5a49945
Mandu <54462907+kartikamilenda@users.noreply.github.com>
2021-03-23 21:24:58 +0700

    add try catch exception

--- CopyingAFile/copyingafile.java
+++ CopyingAFile/copyingafile.java
@@ -15,20 +15,22 @@ import java.util.Scanner;
  */
 public class copyingafile {
     
-    public static void main(String[] args) throws FileNotFoundException{
+    public static void main(String[] args){
         Scanner newFile = new Scanner(System.in);
         System.out.print("Insert your file : ");
         String inputFile = newFile.next();
 
-        
         File file = new File(inputFile);
-        Scanner text = new Scanner(file);
-        String textfile;
-        while(text.hasNextLine()){
+        try{
+            Scanner text = new Scanner(file);
+            String textfile;
+            while(text.hasNextLine()){
             textfile = text.nextLine();
             System.out.println(textfile);
-        }
-        text.close();
-        
+            }
+            text.close();
+        } catch(FileNotFoundException e){
+           System.out.println(inputFile + " File Not Found");
+         }
     } 
 }
