313b89b20aacadf624b7fa09380c30e6db7ee93e
Mandu <54462907+kartikamilenda@users.noreply.github.com>
2021-03-23 21:09:55 +0700

    Mencoba beberapa kali sampai berhasil membaca text

---
+++ CopyingAFile/copyingafile.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package CopyingAFile;
+import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author asus
+ */
+public class copyingafile {
+    
+    public static void main(String[] args) throws FileNotFoundException{
+        Scanner newFile = new Scanner(System.in);
+        System.out.print("Insert your file : ");
+        String inputFile = newFile.next();
+
+        
+        File file = new File(inputFile);
+        Scanner text = new Scanner(file);
+        String textfile;
+        while(text.hasNextLine()){
+            textfile = text.nextLine();
+            System.out.println(textfile);
+        }
+        text.close();
+        
+    } 
+}
