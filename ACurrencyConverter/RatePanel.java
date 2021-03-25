641aeb89681c1682881e69010364bac45ce229b9
Mandu <54462907+kartikamilenda@users.noreply.github.com>
2021-03-25 23:12:02 +0700

    Done?

--- ACurrencyConverter/RatePanel.java
+++ ACurrencyConverter/RatePanel.java
@@ -11,36 +11,44 @@ import javax.swing.*;
  *
  * @author asus
  */
-public class RatePanel {
+public class RatePanel extends JPanel  {
  private double[] rate; // exchange rates 
  private String[] currencyName; 
  private JLabel result;
  private JComboBox comboBox;
+ private JTextField textField;
  // ------------------------------------------------------------ 
  // Sets up a panel to convert cost from one of 6 currencies 
  // into U.S. Dollars. The panel contains a heading, a text 
  // field for the cost of the item, a combo box for selecting 
  // the currency, and a label to display the result. 
  // ------------------------------------------------------------ 
- public RatePanel () 
+ public RatePanel() 
  { 
     JLabel title = new JLabel ("How much is that in dollars?"); 
     title.setAlignmentX (Component.CENTER_ALIGNMENT); 
     title.setFont (new Font ("Helvetica", Font.BOLD, 20));
   
+    textField = new JTextField("Enter number of cost");
+    textField.setColumns(20);
+    
     // Set up the arrays for the currency conversions 
-    currencyName = new String[] {"Select the currency..", 
-    "European Euro", "Canadian Dollar", 
-    "Japanese Yen", "Australian Dollar", 
+    currencyName = new String[] {"Select the currency..","European Euro", "Canadian Dollar", "Japanese Yen", "Australian Dollar", 
     "Indian Rupee", "Mexican Peso"}; 
-    
     rate = new double [] {0.0, 1.2103, 0.7351,0.0091, 0.6969,0.0222, 0.0880}; 
-    result = new JLabel ("-------------");
-    comboBox = new JComboBox<String>(currencyName);
+    result = new JLabel(" ------------ ");
+    // add combo box
+    comboBox = new JComboBox(currencyName);
+    //comboBox.setEditable(true);
+    comboBox.addActionListener(new ComboListener());
     
-    add (title); 
-    add (result); 
- 
+    
+    add(title);
+    add(result); 
+    
+    add(textField);
+    add(comboBox);
+    
  } 
  // ****************************************************** 
  // Represents an action listener for the combo box. 
@@ -54,9 +62,9 @@ public class RatePanel {
  // -------------------------------------------------- 
     public void actionPerformed (ActionEvent event) 
     { 
-    int index = 0; 
-    result.setText ("1 " + currencyName[index] + 
-    " = " + rate[index] + " U.S. Dollars"); 
+    int index = comboBox.getSelectedIndex(); 
+    result.setText(textField.getText() + currencyName[index] + " = " + (rate[index] * Integer.parseInt(textField.getText())) 
+            + " U.S. Dollars"); 
     } 
  } 
 }
