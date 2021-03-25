5e61d82be54e249ec205960bd316a731b876b550
Mandu <54462907+kartikamilenda@users.noreply.github.com>
2021-03-25 18:28:21 +0700

    mencopykan sc yang sudah ada

---
+++ AListofPrimeNumbers/Primes.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AListofPrimeNumbers;
+import java.awt.*; 
+import javax.swing.*;
+/**
+ *
+ * @author asus
+ */
+public class Primes {
+    public static void main (String[] args) 
+   { 
+        JFrame frame = new JFrame("Primes");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        PrimePanel primePanel = new PrimePanel();
+        frame.getContentPane().add(primePanel);
+        frame.pack();
+        frame.setVisible(true);
+ } 
+
+}
